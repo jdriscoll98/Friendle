06d4210f8b96b8bedf94fb46607f39a853e59655
Jack Driscoll <jdriscoll98@ufl.edu>
2022-06-22 17:20:44 -0400

    Fix guess colors

--- src/Pages/Game/index.js
+++ src/Pages/Game/index.js
@@ -47,22 +47,24 @@ export function Game() {
     }
     const board = [];
     const currentGuessRow = guesses.length;
+    const answer = getAnswer();
     // for each row
     for (let i = 0; i < 6; i++) {
       // for each column
       for (let j = 0; j < 5; j++) {
+        const guessStyles = getGuessStyles(guesses[i], answer);
+
         if (i === currentGuessRow) {
           board.push(
-            <div className="box" key={`${i}-${j}-box`}>{`${
+            <div className={`box ${guessStyles[j]}`} key={`${i}-${j}-box`}>{`${
               currentGuess[j] || " "
             }`}</div>
           );
         } else {
           board.push(
-            <div
-              className={`box ${getPreviousGuessStyle(guesses[i], j)}`}
-              key={`${i}-${j}-box`}
-            >{`${guesses[i] ? guesses[i][j] ?? "" : ""}`}</div>
+            <div className={`box ${guessStyles[j]}`} key={`${i}-${j}-box`}>{`${
+              guesses[i] ? guesses[i][j] ?? "" : ""
+            }`}</div>
           );
         }
       }
@@ -70,20 +72,31 @@ export function Game() {
     return board;
   }
 
-  function getPreviousGuessStyle(guess, letterIndex) {
+  function getGuessStyles(guess, word) {
+    const guessStyles = ["", "", "", "", ""];
     if (!guess) {
-      return;
+      return guessStyles;
+    }
+    let wordCopy = word.toUpperCase();
+    const guessCopy = guess.toUpperCase();
+    for (let i = 0; i < 5; i++) {
+      // if letter is in correct position, return green, mark as correct
+      if (guessCopy[i] === wordCopy[i]) {
+        // replace letter with *
+        wordCopy = wordCopy.replace(wordCopy[i], "*");
+        guessStyles[i] = "green";
+      }
     }
 
-    const word = getAnswer().toUpperCase();
-
-    if (word[letterIndex] === guess.toUpperCase()[letterIndex]) {
-      return "green";
-    } else if (word.includes(guess.toUpperCase()[letterIndex])) {
-      return "yellow";
-    } else {
-      return "";
+    for (let i = 0; i < 5; i++) {
+      // if letter is in word, but not in correct position, and not already in the guess return yellow
+      if (wordCopy.includes(guessCopy[i])) {
+        const index = wordCopy.indexOf(guessCopy[i]);
+        wordCopy = wordCopy.replace(wordCopy[index], "*");
+        guessStyles[i] = "yellow";
+      }
     }
+    return guessStyles;
   }
 
   function getAnswer() {
